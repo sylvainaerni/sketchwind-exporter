457821b2b4013542202946f4f64ae7f5182b44ff
Raphi <raphael.antonietti@liip.ch>
2020-06-04 22:35:22 +0200

    update theme

--- src/theme.js
+++ src/theme.js
@@ -2,14 +2,33 @@ export const theme = {}
 theme.colors = {
   transparent:"transparent"
 }
+
 theme.spacing = {
   px: "1px",
   "0": "0"
 }
+
 theme.fontSize = {}
 theme.boxShadow = {
   none:"none"
 }
+
 theme.borderWidth = {
   0:0
 }
+
+theme.opacity = {
+  "0": "0",
+  "10": "0.1",
+  "20": "0.2",
+  "30": "0.3",
+  "40": "0.4",
+  "50": "0.5",
+  "60": "0.6",
+  "70": "0.7",
+  "80": "0.8",
+  "90": "0.9",
+  "100": "1"
+},
+
+theme.backgroundColor = theme.colors
