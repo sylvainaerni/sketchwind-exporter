f977858d57a93a6d9d319a8849682ecd20a1b02a
Sylvain Aerni <sylvain.aerni@gmail.com>
2020-06-10 23:00:12 +0200

    reset double quotes

--- src/theme.js
+++ src/theme.js
@@ -1,19 +1,19 @@
 export const theme = {};
 theme.colors = {
-  transparent: 'transparent'
+  transparent: "transparent"
 };
 
 theme.spacing = {
-  px: '1px',
-  '0': '0',
+  px: "1px",
+  "0": "0",
 };
 
 theme.screens = {};
 
 theme.fontSize = {};
 theme.boxShadow = {
-  none: 'none',
-  inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
+  none: "none",
+  inner: "inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)",
 };
 
 theme.borderWidth = {
@@ -21,20 +21,20 @@ theme.borderWidth = {
 };
 
 theme.opacity = {
-  '0': '0',
-  '10': '0.1',
-  '20': '0.2',
-  '30': '0.3',
-  '40': '0.4',
-  '50': '0.5',
-  '60': '0.6',
-  '70': '0.7',
-  '80': '0.8',
-  '90': '0.9',
-  '100': '1',
+  "0": "0",
+  "10": "0.1",
+  "20": "0.2",
+  "30": "0.3",
+  "40": "0.4",
+  "50": "0.5",
+  "60": "0.6",
+  "70": "0.7",
+  "80": "0.8",
+  "90": "0.9",
+  "100": "1",
 }
 
 theme.borderRadius = {
-  full:"9999999px",
+  full:"9999px",
   half:"50%"
 }
